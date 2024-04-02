5ee815f56a5c8d418db1c2a10309aabcac4876af
anvarturakulov <anvarsam@mail.ru>
2024-04-02 07:11:39 +0500

    edit uri

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -7,20 +7,21 @@ import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
-  @UsePipes(new ValidationPipe())
-  @Post('register')
-  async register(@Body() dto: AuthDto) {
-    const oldUser = await this.authService.findUser(dto.login);
-    if (oldUser) {
-      throw new BadRequestException(ALREADY_REGISTERED_ERROR)
-    }
-    return this.authService.createUser(dto);
-  }
+  // @UsePipes(new ValidationPipe())
+  // @Post('register')
+  // async register(@Body() dto: AuthDto) {
+  //   const oldUser = await this.authService.findUser(dto.login);
+  //   if (oldUser) {
+  //     throw new BadRequestException(ALREADY_REGISTERED_ERROR)
+  //   }
+  //   return this.authService.createUser(dto);
+  // }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
   async login(@Body() {login, password}: AuthDto) {
+    console.log('sssss')
     const { email, role, name, storageId, productId } = await this.authService.validateUser(login, password);
     return this.authService.login(email, role, name, storageId, productId);
   }
