bf80f9c7f6730093d404f9edf98374eb07fb1bfc
anvarturakulov <anvarsam@mail.ru>
2024-04-15 09:20:38 +0500

    fix font size in select

--- frontend/app/components/common/header/header.tsx
+++ frontend/app/components/common/header/header.tsx
@@ -76,7 +76,7 @@ export default function Header({ windowFor ,className, ...props }: HeaderProps):
                                     }
                                     }
                                 }} 
-                        />
+                            />
                         </>
                     }
                     </div>
