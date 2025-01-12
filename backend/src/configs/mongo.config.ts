ada0751897bc9af1392119cc84fe5e7e14532a25
Anvar Turakulov <anvarsam@mail.ru>
2025-01-12 17:17:04 +0500

    shavkatbase

--- backend/src/configs/mongo.config.ts
+++ backend/src/configs/mongo.config.ts
@@ -2,15 +2,15 @@ import { ConfigService } from '@nestjs/config';
 
 const getMongoString = (configService: ConfigService) => 
   'mongodb://'+
-  configService.get('MONGO_LOGIN')+
+  configService.get('MONGO_LOGIN2')+
   ':'+
-  configService.get('MONGO_PASSWORD')+
+  configService.get('MONGO_PASSWORD2')+
   '@'+
-  configService.get('MONGO_HOST') +
+  configService.get('MONGO_HOST2') +
   ':' +
-  configService.get('MONGO_PORT');
+  configService.get('MONGO_PORT2');
   // '/'+
-  // configService.get('MONGO_AUTHDATABASE');
+  // configService.get('MONGO_AUTHDATABASE2');
 
 const getMongoOptions = () => ({
   useNewUrlParser: true,
