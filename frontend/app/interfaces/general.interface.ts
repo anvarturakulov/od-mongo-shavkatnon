13c0da3da998209ff221c237de3954294566d7ae
Anvar Turakulov <anvarsam@mail.ru>
2025-01-22 13:47:32 +0500

    fix adding userRole ZP

--- frontend/app/interfaces/general.interface.ts
+++ frontend/app/interfaces/general.interface.ts
@@ -52,6 +52,6 @@ export interface DefinedTandirWorkers {
     thirdWorker: string | null,
 }
 
-export const dashboardUsersList = [UserRoles.ADMIN, UserRoles.HEADCOMPANY, UserRoles.GUEST, UserRoles.GLBUX, UserRoles.ZAMGLBUX];
+export const dashboardUsersList = [UserRoles.ADMIN, UserRoles.HEADCOMPANY, UserRoles.GUEST, UserRoles.GLBUX, UserRoles.ZAMGLBUX, UserRoles.ZP];
 export const workersUsersList = [UserRoles.DELIVERY, UserRoles.SELLER, UserRoles.ELAKCHI, UserRoles.HAMIRCHI, UserRoles.HEADSECTION, UserRoles.TANDIR] 
 export const adminAndHeadCompany = [UserRoles.ADMIN, UserRoles.HEADCOMPANY, UserRoles.GLBUX]
